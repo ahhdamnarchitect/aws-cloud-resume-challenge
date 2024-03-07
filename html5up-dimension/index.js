8fde85828524885546fa9905a04698c50f68c3d9
Adam Grant <adam.grant8@gmail.com>
2024-03-07 08:11:01 -0600

    Initial Commit

---
+++ html5up-dimension/index.js
@@ -0,0 +1,9 @@
+// JavaScript Code
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://qkekwudeiefymbzqoetoryrvhi0vwuip.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
